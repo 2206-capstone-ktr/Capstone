a5b1af67e72b52f709c3f061e87ef2b330e6fab1
Ty Kuper <tykuper@gmail.com>
2022-08-23 14:45:10 -0700

    link to currentItin

--- client/components/ActiveItinCard.js
+++ client/components/ActiveItinCard.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function ActiveItinCard(props) {
   const itin = props.itin;
 
   return (
-    <a
-      href='#'
+    <Link
+      onClick={() => props.onClick(itin)}
+      to={'/currentItinerary'}
       key={itin.id}
       className='flex flex-col items-center m-5 bg-white rounded-lg border shadow-lg md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'
     >
       <img
         className='object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg'
         src='favicon.ico'
-        alt=''
+        alt='vacation image'
       />
       <div className='flex flex-col justify-between p-4 leading-normal'>
         <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
@@ -28,7 +30,7 @@ function ActiveItinCard(props) {
           {itin.startDate} to {itin.endDate}
         </p>
       </div>
-    </a>
+    </Link>
   );
 }
 
--- client/views/activeItinerariesView.js
+++ client/views/activeItinerariesView.js
@@ -3,27 +3,9 @@ import { connect, useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import ActiveItinCard from '../components/ActiveItinCard';
 import { fetchItineraries } from '../store/itinerary';
+import { fetchItinerary } from '../store/singleItinerary';
 
 export const activeItinerariesView = (props) => {
-  const itins = [
-    {
-      id: 1,
-      name: 'Vacation',
-      city: 'Los Angeles',
-      description: `My vacation in LA`,
-      startDate: '2022-09-29',
-      endDate: '2022-10-02',
-    },
-    {
-      id: 2,
-      name: 'Island Hopping',
-      city: 'Honolulu',
-      description: `Island hopping between Hawai'i, O'Ahu, Maui, and Kauai`,
-      startDate: '2022-11-06',
-      endDate: '2022-11-13',
-    },
-  ];
-
   const user = useSelector((state) => state.auth);
   const itineraries = useSelector((state) => state.itinerary);
 
@@ -32,6 +14,10 @@ export const activeItinerariesView = (props) => {
   useEffect(() => {
     dispatch(fetchItineraries(user.id));
   }, []);
+
+  const onClickHandler = async (itinerary) => {
+    dispatch(fetchItinerary(itinerary.id));
+  };
 
   return (
     <div>
@@ -42,7 +28,7 @@ export const activeItinerariesView = (props) => {
       </div>
       <div className='flex justify-center flex-col'>
         {itineraries?.map((itin) => (
-          <ActiveItinCard key={itin.id} itin={itin} />
+          <ActiveItinCard key={itin.id} itin={itin} onClick={onClickHandler} />
         ))}
       </div>
     </div>
@@ -50,54 +36,3 @@ export const activeItinerariesView = (props) => {
 };
 
 export default activeItinerariesView;
-
-//------------------------------
-// import React from 'react';
-// import { fetchItineraries } from '../../store/Itinerary';
-// import Test from './test';
-// import { connect } from 'react-redux';
-// import { Link } from 'react-router-dom';
-// export class DisplayItinerary extends React.Component {
-//   componentDidMount() {
-//     this.props.fetchItineraries(this.props.user.id);
-//     console.log('hereeeeeeee', this.props);
-//   }
-
-//   render() {
-//     let userItineraries = this.props.itineraries ? this.props.itineraries : [];
-
-//     return (
-//       <div>
-//         <div>
-//           {userItineraries.length ? (
-//             <div>
-//               <h2>Your Itinerary is:</h2>
-//               {userItineraries.map((itinerary) => (
-//                 <div key={itinerary.id}>
-//                   <div>{itinerary.name}</div>
-//                   <form>
-//                     <button type='submit'>Delete</button>
-//                   </form>
-//                   <br />
-//                 </div>
-//               ))}
-//             </div>
-//           ) : (
-//             <h2 className='textColor'>Your Itinerary is empty</h2>
-//           )}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-// const mapState = (state) => {
-//   return {
-//     itineraries: state.itineraryReducer,
-//   };
-// };
-// const mapDispatch = (dispatch, { history }) => {
-//   return {
-//     fetchItineraries: (userId) => dispatch(fetchItineraries(userId)),
-//   };
-// };
-// export default connect(mapState, mapDispatch)(DisplayItinerary);
