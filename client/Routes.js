283bb5ab63d3abfe79973b3b6e8f440df18e0f38
Rigoberto <rigobertobgomez@gmail.com>
2022-08-24 03:55:27 +0900

    merged changes and hooks

--- client/Routes.js
+++ client/Routes.js
@@ -1,65 +1,68 @@
 import React, { Component, Fragment } from 'react';
-import { connect } from 'react-redux';
+import { connect, useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
 import CreateItinerary from './components/Forms/CreateItin';
 import { Login } from './components/Forms/Login';
 import { SignUp } from './components/Forms/SignUp';
 import Home from './components/Home';
-import { DisplayItineraries } from './components/Iteneraries/Itineraries';
+
 import { me } from './store';
 import activeItinerariesView from './views/activeItinerariesView';
 
 /**
  * COMPONENT
  */
-class Routes extends Component {
-  componentDidMount() {
-    this.props.loadInitialData();
-  }
+const Routes = () => {
+  const isLoggedIn = useSelector((state) => !!state.auth.id);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(me());
+  }, []);
 
-  render() {
-    const { isLoggedIn, userId } = this.props;
+  // const { isLoggedIn, userId } = this.props;
 
-    return (
-      <div>
-        {isLoggedIn ? (
-          <Switch>
-            <Route path='/home' component={Home} />
-            <Route path='/create' component={CreateItinerary} />
-            <Route path='/users/itineraries' component={DisplayItineraries} />
-            <Redirect to='/home' />
-          </Switch>
-        ) : (
-          <Switch>
-            <Route path='/login' component={Login} />
-            <Route path='/signup' component={SignUp} />
-            <Route path='/' exact component={Login} />
-          </Switch>
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      {isLoggedIn ? (
+        <Switch>
+          <Route path='/home' component={Home} />
+          <Route path='/create' component={CreateItinerary} />
+
+          <Route path='/activeitineraries' component={activeItinerariesView} />
+          <Redirect to='/home' />
+        </Switch>
+      ) : (
+        <Switch>
+          <Route path='/login' component={Login} />
+          <Route path='/signup' component={SignUp} />
+          <Route path='/' exact component={Login} />
+        </Switch>
+      )}
+    </div>
+  );
+};
 
 /**
  * CONTAINER
  */
-const mapState = (state) => {
-  return {
-    // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
-    // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
-    isLoggedIn: !!state.auth.id,
-  };
-};
+// const mapState = (state) => {
+//   return {
+//     // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
+//     // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
+//     isLoggedIn: !!state.auth.id,
+//   };
+// };
 
-const mapDispatch = (dispatch) => {
-  return {
-    loadInitialData() {
-      dispatch(me());
-    },
-  };
-};
+// const mapDispatch = (dispatch) => {
+//   return {
+//     loadInitialData() {
+//       dispatch(me());
+//     },
+//   };
+// };
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
-export default withRouter(connect(mapState, mapDispatch)(Routes));
+// export default withRouter(connect(mapState, mapDispatch)(Routes));
+export default Routes;
