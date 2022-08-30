e1691f8c6864bc3044cda584f737f2b6def33104
Rigoberto <rigobertobgomez@gmail.com>
2022-08-31 06:05:48 +0900

    filtered views working

--- server/api/googlemaps.js
+++ server/api/googlemaps.js
@@ -1,24 +1,25 @@
 import axios from 'axios';
 
-const URL =
-  'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
-
-export const getPlacesData = async (sw, ne) => {
+export const getPlacesData = async (type, sw, ne) => {
   try {
     const {
       data: { data },
-    } = await axios.get(URL, {
-      params: {
-        bl_latitude: sw.lat,
-        tr_latitude: ne.lat,
-        bl_longitude: sw.lng,
-        tr_longitude: ne.lng,
-      },
-      headers: {
-        'X-RapidAPI-Key': 'b58431f263msh802d9f5742c878ap19e9b9jsn61a7b7986493',
-        'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
-      },
-    });
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          tr_latitude: ne.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+        },
+        headers: {
+          'X-RapidAPI-Key':
+            'b58431f263msh802d9f5742c878ap19e9b9jsn61a7b7986493',
+          'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
+        },
+      }
+    );
     console.log(data);
     return data;
   } catch (err) {
