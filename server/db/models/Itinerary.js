5e90f2279e984fb6d95942bfad8d93df24587bb3
Ty Kuper <tykuper@gmail.com>
2022-09-03 14:11:23 -0700

    updated itinerary model to include days. Updted pagination to refer to days. Updated DnD issue to locally store DnD function. Still need to save back to store/kick off thunks.

--- server/db/models/Itinerary.js
+++ server/db/models/Itinerary.js
@@ -14,13 +14,24 @@ const Itinerary = db.define('itinerary', {
     type: Sequelize.STRING,
   },
   startDate: {
-    type: Sequelize.STRING,
+    type: Sequelize.DATEONLY,
     allowNull: false,
   },
   endDate: {
-    type: Sequelize.STRING,
+    type: Sequelize.DATEONLY,
     allowNull: false,
   },
+  days: {
+    type: Sequelize.INTEGER,
+  },
 });
+
+Itinerary.beforeValidate(
+  (itin) =>
+    (itin.days = Math.ceil(
+      (Date.parse(itin.endDate) - Date.parse(itin.startDate)) /
+        (1000 * 60 * 60 * 24)
+    ))
+);
 
 module.exports = Itinerary;
