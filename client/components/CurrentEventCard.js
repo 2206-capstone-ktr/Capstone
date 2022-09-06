ed7556caf9538a263533f2abb0ff4f76b0657fdf
Kevin Zhang <kzkevin123@gmail.com>
2022-09-06 17:34:12 -0400

    changed eventcard

--- client/components/CurrentEventCard.js
+++ client/components/CurrentEventCard.js
@@ -18,10 +18,10 @@ function CurrentEventCard(props) {
   return (
     <div
       key={event.id}
-      className='flex flex-col items-center m-5 bg-white rounded-lg border shadow-lg md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'
+      className='flex flex-col items-center m-5 bg-gray-100 rounded-lg border shadow-lg md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'
     >
       <img
-        className='object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg'
+        className='object-cover w-full h-200 rounded-t-xl md:h-auto xl:w-48 xl:rounded-none xl:rounded-l-lg'
         src={event.imageUrl}
         alt=''
       />
@@ -29,6 +29,13 @@ function CurrentEventCard(props) {
         <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
           {event.name}
         </h5>
+        <h6 className=' text-2lg font-medium  text-gray-900 dark:text-white'>
+          {event.address}
+        </h6>
+        <br></br>
+        <h6 className=' text-2lg font-medium  text-gray-900 dark:text-white'>
+          Phone: {event.phone}
+        </h6>
         <button className='btn btn-blue' onClick={handleClick}>
           Delete
         </button>
